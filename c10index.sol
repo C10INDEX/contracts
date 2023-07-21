de3de06a1e3849f6ce1c43c3e23d20fc1d6d2ed9
ks <ksainte19@gmail.com>
2023-07-22 01:25:31 +0200

    swap work with uni but check oneinch!

--- c10index.sol
+++ c10index.sol
@@ -5,6 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./vault.sol";
 
 contract C10Token is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("C10index", "C10") {}
@@ -20,33 +22,88 @@ contract C10Token is ERC20, ERC20Burnable, Ownable {
 
 contract C10INDEX is C10Token {
 
-      address public constant LINK = 0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29;//LINK Mumbai faucet
+    //address public constant LINK = 0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29;//LINK Mumbai faucet
+    //router pour uniswap qui work,mais a check pour oneinch!
+    address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
 
-    //implementation of splitter => later set with proportions?
-     function splitUsdcForBuy(uint256 usdcAmount) public pure returns (uint256) {
-        uint256 usdcForToken = usdcAmount / 2;
-        return usdcForToken;
+    address public constant USDC = 0x65aFADD39029741B3b8f0756952C74678c9cEC93;
+    IERC20 public usdcToken = IERC20(USDC);
+    C10Vault public vault;//CONTRACT DU VAULT
+
+    //Chainlink data's array
+    AggregatorV3Interface[] internal priceFeeds;
+    //Assets structure array
+    Asset[2] public assets;
+
+    struct Asset 
+    {
+        uint256 price;
     }
 
-    // swap proto USDC for LINK => to do with Oneinch
-    function swap(uint256 amountIn) public returns (uint256 amountOut) {
+    function setVaultAddress(address _vault) public onlyOwner {
+        vault = C10Vault(_vault);
     }
-    //will be used to deposit tokens in the contract from our MM,first when deploying approve contract on USDC approve fct
-    function Deposit_Token(uint amount) external {
-        IERC20 token = IERC20(0xe9DcE89B076BA6107Bb64EF30678efec11939234);//USDC MUMBAI FAUCET
-        //0xF14f9596430931E177469715c591513308244e8F DAI FAUCET
-        require(token.allowance(msg.sender, address(this)) >= amount, "error");
-        
-        require(token.transferFrom(msg.sender, address(this), amount), "error");
-    }
-    //check if the swap works properly
-     function buyETF(uint256 usdcAmount) external {
 
-        uint256 usdcsplitted = splitUsdcForBuy(usdcAmount);
-        for ( i = 0; i < 2; i++){
-            swap(usdcsplitted);
+    function changeVaultOwner(address _vaultOwner) public onlyOwner {
+        vault.setC10ContractAsOwner(_vaultOwner);
+    }
+
+    //["0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29","0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29"]
+    function setTokenAddresses(address[2] memory _tokenAddresses) public onlyOwner {
+        tokenAddresses = _tokenAddresses;
+    }
+
+    function setProportions(uint[2] memory _Proportion) public onlyOwner {
+        Proportion = _Proportion;
+    }
+
+    function updateAssetValues() public 
+    {
+        for (i = 0; i < priceFeeds.length; i++) 
+        {
+            (, int256 price, , , ) = priceFeeds[i].latestRoundData();
+            assets[i].price = uint256(price);
         }
-        mint(usdcAmount * 1e18);
-        //=> next goal : TRANSFER TO VAULT
+    }
+    //["0x48731cF7e84dc94C5f84577882c14Be11a5B7456","0x48731cF7e84dc94C5f84577882c14Be11a5B7456"]
+    function set_Oracle (address[] memory _priceFeedAddresses) public{
+        for (i = 0; i < _priceFeedAddresses.length; i++)
+        {
+            AggregatorV3Interface priceFeed = AggregatorV3Interface(_priceFeedAddresses[i]);
+            priceFeeds.push(priceFeed);
+        }
+        updateAssetValues();
+    }
+
+    //test avec uniswap par simpliciter de doc, a demander a oneinch team pour unoswap?
+    function swap(uint256 amountIn) internal returns (uint256 amountOut)
+    {
+        usdcToken.approve(address(swapRouter), amountIn);
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: USDC,
+                tokenOut: tokenAddresses[i],
+                fee: poolFee,
+                recipient: address(vault),
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+        amountOut = swapRouter.exactInputSingle(params);     
+    }
+
+    //dont need deposit function anymore,but need to approve spending in usdc contract
+    function buyETF(uint256 usdcAmount) external {    
+        
+        
+        require(usdcToken.allowance(msg.sender, address(this)) >= usdcAmount, "Error");
+        require(usdcToken.transferFrom(msg.sender, address(this), usdcAmount), "Error.");
+        //the swap sends values to the vault
+        for ( i = 0; i < 2; i++){
+            swap(Proportion[i]*usdcAmount /100);
+        }
+        //mint(usdcAmount * 1e18);  
     }
 }
