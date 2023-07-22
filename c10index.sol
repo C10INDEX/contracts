d43c57e0418a57a8eb2aed300d3918c9ab2b8184
larfef <120338027+larfef@users.noreply.github.com>
2023-07-22 19:34:54 +0000

    Update c10index.sol
    Remove uniswap stuff cause were switching on gnosis blockchain. Add data feed from chainlink using crosschain mailbox.

--- c10index.sol
+++ c10index.sol
@@ -1,15 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.21;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-import "./vault.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "./lastvault.sol";
+
+/* Price feeds addresses
+0xd6d95CF12EdC1C513beF08E78f79bA31E516FF32 ETH/USD
+0xF3e0f39954Fc5E3154a400aA29133438382b28A1 BTC/USD
+0x30c5f72273cB67EF95257d77d2E7ae9972aa9d4D MATIC/USD 
+0xfB425EF72d1bBDe50195784CC6049d6d486349f8 DAI/USD
+1INCH/USD
+0x8B83c43d30944B43Fd976440031C21E84bdd3b8e USDT/USD
+0xdaa1ED784644db147cd20F3324E9676295165370 LINK/USD
+0x338F2170c43D8111Bad6B1883e473de00695372E BNB/USD
+0x7FC9a50aE58b83c202CDb7a0bD9b3705C4067471 SNX/USD
+0x3c8AB2F01ED2E6F06b8a2568472F2Db12CC369b7 UNI/USD
+*/
 
 contract C10Token is ERC20, ERC20Burnable, Ownable {
     constructor() ERC20("C10index", "C10") {}
@@ -23,72 +36,84 @@ contract C10Token is ERC20, ERC20Burnable, Ownable {
     }
 }
 
-contract C10INDEX is C10Token {
+contract C10ETF is C10Token, C10Vault {
 
-    //address public constant LINK = 0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29;//LINK Mumbai faucet
-    //router pour uniswap qui work,mais a check pour oneinch!
-    address public constant routerAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
     using SafeMath for uint256;
 
-    address public constant USDC = 0x65aFADD39029741B3b8f0756952C74678c9cEC93;
-    address public constant LINK = 0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29;
-    IERC20 public usdcToken = IERC20(USDC);
-    IERC20 public linkToken = IERC20(LINK);
-    address[2] public tokenAddresses;
-    uint256[2] public Proportion;
-    uint256 public i = 0;
-    uint256 public j = 0;
+    Asset[10] public assets;
+    AggregatorV3Interface[] internal priceFeeds;
+
+    IERC20 public DAIToken = IERC20(DAI);
+    address public constant routerAddress = 0xA818b4F111Ccac7AA31D0BCc0806d64F2E0737D7;
+    address public constant DAI = 0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d;
+    address [10] public tokenAddresses;
+    
+    C10Vault public vault;
+
+    mapping (address => uint256) public tokenBalances;
+    
+    uint256 internal i = 0;
+    uint256 internal j = 0;
     uint256 public AUM_In_Usd;
     uint256 public C10_Supply;
     uint256 public C10_Price;
-
-
-    C10Vault public vault;//CONTRACT DU VAULT
-
-    //Chainlink data's array
-    AggregatorV3Interface[] internal priceFeeds;
-    //Assets structure array
-    Asset[2] public assets;
+    uint256 public Pool_Value;
+    uint256 public tokenBalance;
+    uint256 public tokenPrice;
+    uint24 public constant poolFee = 3000;
+    uint256 [10] public proportion;
+    uint256[10] public decimal = [18, 8, 18, 6, 18, 18, 18, 18, 18, 18];
 
     event transaction_success(uint256 value);
+
     struct Asset 
     {
         uint256 price;
-        uint256 proportion;
+        uint256 part;
         uint256 dec;
     }
-    //aller plus vite
-    constructor() {
-    Proportion[0] = 75;
-    Proportion[1] = 25;
-    priceFeeds = new AggregatorV3Interface[](2);
-    priceFeeds[0] = AggregatorV3Interface(0x48731cF7e84dc94C5f84577882c14Be11a5B7456); //LINK/USD price feed
-    priceFeeds[1] = AggregatorV3Interface(0x48731cF7e84dc94C5f84577882c14Be11a5B7456); //LINK/USD
-    tokenAddresses[0] = 0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29;//link
-    tokenAddresses[1] = 0xe9c4393a23246293a8D31BF7ab68c17d4CF90A29;
-    }
 
+    constructor() {
+    tokenAddresses[0] = 0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1;//WETH
+    tokenAddresses[1] = 0x8e5bBbb09Ed1ebdE8674Cda39A0c169401db4252;//WBTC
+    tokenAddresses[2] = 0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d;//DAI
+    tokenAddresses[3] = 0x7122d7661c4564b7C6Cd4878B06766489a6028A2;//WMATIC
+    tokenAddresses[4] = 0x4ECaBa5870353805a9F068101A40E0f32ed605C6;//USDT
+    tokenAddresses[5] = 0xE2e73A1c69ecF83F464EFCE6A5be353a37cA09b2;//LINK
+    tokenAddresses[6] = 0xCa8d20f3e0144a72C6B5d576e9Bd3Fd8557E2B04;//BNB
+    tokenAddresses[7] = 0x4537e328Bf7e4eFA29D05CAeA260D7fE26af9D74;//UNI
+    tokenAddresses[8] = 0x7f7440C5098462f833E123B44B8A03E1d9785BAb;//W1INCH
+    tokenAddresses[9] = 0x3A00E08544d589E19a8e7D97D0294331341cdBF6;//SNX18
+    
+    }
     function setVaultAddress(address _vault) public onlyOwner {
         vault = C10Vault(_vault);
     }
 
     function changeVaultOwner(address _vaultOwner) public onlyOwner {
         vault.setC10ContractAsOwner(_vaultOwner);
-        
+    }
+
+    function getTotalSupply() public view returns (uint256) {
+    return totalSupply();
     }
 
     function mint_single(uint amount) public {
-         mint(amount * 1e18);
+         mint(amount);
     }
 
-    function get_supply() public view returns (uint256) {
-        return totalSupply();
-    }
-    
-    struct Asset 
+    function set_Proportion(uint256[] memory _proportion) public
     {
-        uint256 price;
+        for (i = 0; i < proportion.length; i++){
+        proportion[i] = _proportion[i];
+        }
+    }
+
+    function setPriceFeeds (address[] memory _PriceFeeds) public
+    {
+        for  (i = 0; i < _PriceFeeds.length; i++){
+            priceFeeds[i] = AggregatorV3Interface(_PriceFeeds[i]);
+        }
     }
 
     function updateAssetValues() public 
@@ -97,115 +122,35 @@ contract C10INDEX is C10Token {
         {
             (, int256 price, , , ) = priceFeeds[i].latestRoundData();
             assets[i].price = uint256(price);
+            assets[i].dec = decimal[i];
         }
     }
-    //["0x48731cF7e84dc94C5f84577882c14Be11a5B7456","0x48731cF7e84dc94C5f84577882c14Be11a5B7456"]
-    // function set_Oracle (address[] memory _priceFeedAddresses) public{
-    //     for (i = 0; i < _priceFeedAddresses.length; i++)
-    //     {
-    //         AggregatorV3Interface priceFeed = AggregatorV3Interface(_priceFeedAddresses[i]);
-    //         priceFeeds.push(priceFeed);
-    //     }
-    //     updateAssetValues();
-    // }
-
-    //test avec uniswap par simpliciter de doc, a demander a oneinch team pour unoswap?
-    function buy_swap(uint256 amountIn) internal returns (uint256 amountOut)
-    {
-        usdcToken.approve(address(swapRouter), amountIn);
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
-            .ExactInputSingleParams({
-                tokenIn: USDC,
-                tokenOut: tokenAddresses[i],
-                fee: poolFee,
-                recipient: address(vault),
-                deadline: block.timestamp,
-                amountIn: amountIn,
-                amountOutMinimum: 0,
-                sqrtPriceLimitX96: 0
-            });
-        amountOut = swapRouter.exactInputSingle(params);     
-    }
-    //mieux qu une struct
-    mapping (address => uint256) public tokenBalances;
 
     function _updateTokenBalance() public{
 
-        for (i = 0; i < tokenAddresses.length; i++){
-            uint256 nextBalance = IERC20(tokenAddresses[i]).balanceOf(address(vault));//with decimals
+        for (i = 0; i < 1; i++){
+            uint256 nextBalance = IERC20(tokenAddresses[i]).balanceOf(address(vault));//with 18 decimals
             tokenBalances[tokenAddresses[i]] = nextBalance;
         }
     }
 
-    uint256 public Pool_Value;
+    function _updatePoolValue() public returns (uint256) {
+    Pool_Value = 0; 
 
-    function _updatePoolValue() public returns(uint256){
-        
-        Pool_Value = 0;
-        _updateTokenBalance();// with decimals
-        updateAssetValues();
-        for (i = 0; i < tokenAddresses.length; i++) 
-        {
-            uint256 tokenBalance = tokenBalances[tokenAddresses[i]];
-            uint256 tokenPrice = assets[i].price /1e9;
-            Pool_Value = Pool_Value.add(tokenBalance.mul(tokenPrice));
-        }
-        return Pool_Value;
+    _updateTokenBalance(); 
+    updateAssetValues();
+    for (i = 0; i < 1; i++) {
+        tokenBalance = (tokenBalances[tokenAddresses[i]]);//with 18 decimals
+        tokenPrice = (assets[i].price / 1e8);
+        Pool_Value = Pool_Value.add(tokenBalance.mul(tokenPrice));
+    }
+    return Pool_Value;
     }
 
-
-    uint256 public AUM_In_Usd;
-    uint256 public C10_Supply;
-
-    function buyETF(uint256 usdcAmount) public  {    
-        IERC20 token = IERC20(USDC);
-        uint256 C10_Price;
-    
-        require(token.allowance(msg.sender, address(this)) >= usdcAmount, "Error");
-        require(token.transferFrom(msg.sender, address(this), usdcAmount), "Error.");
-        AUM_In_Usd = _updatePoolValue();
-        C10_Supply = totalSupply();
-        if (AUM_In_Usd == 0 && C10_Supply == 0){
-            C10_Price = 1000000;//10 dollars de base;
-        }
-        else {
-            C10_Price = AUM_In_Usd / C10_Supply;
-        }
-        for (i = 0; i < 2; i++){
-            buy_swap(Proportion[i]*usdcAmount /100);
-        }
-        mint((usdcAmount / C10_Price)* 1e18);
-    }
-
-    function sellETF(uint256 amount) public {
-        
-        address recipient = msg.sender;
-        
-        for (i = 0; i < priceFeeds.length; i++) 
-        {
-            (, int256 price, , , ) = priceFeeds[i].latestRoundData();
-            assets[i].price = uint256(price);
-        }
-        for (i = 0; i < 2; i++) {
-            uint256 amountToWithdraw = (amount * Proportion[i] / 100) / assets[i].price / 1e9;
-            vault.withdraw(tokenAddresses[i], amountToWithdraw);
-        }
-        i = 0;
-        for (j = 0; j < 2; j++) {
-            uint256 amountToSwap = (amount * Proportion[j] / 100) / assets[j].price / 1e9;
-            sell_swap(recipient,amountToSwap);
-        }
-         //emit transaction_success(amount);
-    }
-
-    function get_c10()public view returns(uint256){
+    function getc10Price()public view returns(uint256){
         return AUM_In_Usd/ totalSupply();
     }
 
-    function get_TVL()public view returns(uint256){
+    function getTVL()public view returns(uint256){
         return AUM_In_Usd;
     }
-
-
-
-}
